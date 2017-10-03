41ab3abd6afe87697f059ee58a10df37bb1523b3
Abeera1234 <31432329+Abeera1234@users.noreply.github.com>
2017-10-03 09:27:08 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int num = 2;
+            Console.WriteLine("---- First 10 Prime Number ----");
+
+            while (count != 10)
+            {
+                if (num % 2 != 0)
+                {
+                    Console.WriteLine("Prime : " + num);
+                    count++;
+                }
+                num++;
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
+   
